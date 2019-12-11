9ac04b7a272b4d0a1777b4fcdb971d5d6d0ee759
CelineBuff <celine.buffat@etu.univ-st-etienne.fr>
2019-12-11 17:05:30 +0100

    Remise en forme des données + trouver les stations près de moi avec coordonnées rentrés à la main

--- SemWebProject/src/main/java/com/semweb/Controller/MainController.java
+++ SemWebProject/src/main/java/com/semweb/Controller/MainController.java
@@ -16,6 +16,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class MainController {
 
+    @RequestMapping(value = "/")
+    public String goToMainPage(Model m) {
+        List<City> cities = CityDAO.getAllCity();
+        m.addAttribute("cities", cities);
+        return "/cities.html";
+    }
+
     @RequestMapping(value = "/cities")
     public String goToTownsPage(Model m) {
         List<City> cities = CityDAO.getAllCity();
@@ -28,6 +35,8 @@ public class MainController {
         List<Station> stations = new ArrayList<>();
         stations = StationDAO.getAllStationByCityName(cityName);
         m.addAttribute("stations", stations);
+        m.addAttribute("cityName", cityName);
+
         return "/stations.html";
     }
 
@@ -39,9 +48,19 @@ public class MainController {
     }
 
     @RequestMapping(method = RequestMethod.POST, path = "/search")
-    public String search(Model m,  @RequestParam(value = "searchString") String searchString) {
+    public String search(Model m, @RequestParam(value = "searchString") String searchString) {
         List<Station> stations = new ArrayList<>();
         stations = StationDAO.findStation(searchString);
+        m.addAttribute("stations", stations);
+        return "/stations.html";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/searchStationNearMe")
+    public String searchStationNearMe(Model m) {
+        Double lat = 45.453254d;
+        Double lng = 4.390432d;
+        List<Station> stations = new ArrayList<>();
+        stations = StationDAO.findStationNearMe(lat, lng);
         m.addAttribute("stations", stations);
         return "/stations.html";
     }
